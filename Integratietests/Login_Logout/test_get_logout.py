b4fe3eb8b299b22b9f9c9d2e536bcf362510f93a
TigranesTG <job.limbeek@gmail.com>
2025-10-10 12:58:00 +0200

    renamed files again

--- Integratietests/Login_Logout/test_get_logout.py
+++ Integratietests/Login_Logout/test_get_logout.py
@@ -1,43 +1,4 @@
-import pytest
 import requests
-
-
-@pytest.fixture
-def login_as_admin():
-
-    url = "http://localhost:8000"
-    login_url = url + 'login'
-
-    login_response = requests.post(
-        login_url, json={"username": "Job", "password": "sendhelp"})
-
-    assert login_response.status_code == 200
-    session_token = login_response.json().get('session_token')
-    assert session_token is not None
-
-    return {
-        'url': url,
-        'session_token': session_token
-    }
-
-
-@pytest.fixture
-def login_as_user():
-
-    url = "http://localhost:8000"
-    login_url = url + 'login'
-
-    login_response = requests.post(
-        login_url, json={"username": "Job", "password": "sendhelp"})
-
-    assert login_response.status_code == 200
-    session_token = login_response.json().get('session_token')
-    assert session_token is not None
-
-    return {
-        'url': url,
-        'session_token': session_token
-    }
 
 # Checks if a admin token is not valid
 
