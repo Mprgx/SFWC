c3c91eb0f4bf05e7658cae388939cdea4b2e786b
Sharad Randjitsing <jurianrandjitsing@gmail.com>
2025-10-17 14:32:39 +0200

    created api, register, login with JWT and refresh tokens

---
+++ MobyPark/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MobyPark.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public DateTimeOffset CreatedAt { get; set; }
+        public int BirthYear { get; set; }
+        public UserRole Role { get; set; } = UserRole.Customer;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string? RefreshToken { get; set; }
+        public DateTimeOffset? RefreshTokenExpiryTime { get; set; }
+    }
+}
---
+++ MobyPark/Models/UserReadDto.cs
@@ -0,0 +1,6 @@
+﻿using MobyPark.Entities;
+
+namespace MobyPark.Models
+{
+    public record UserReadDto(Guid Id, string Username, string Name, string Email, string PhoneNumber, int BirthYear, UserRole Role, DateTimeOffset CreatedAt);
+}
