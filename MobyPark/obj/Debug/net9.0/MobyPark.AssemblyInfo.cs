457f225f6f9655d3763e5a3a7b5a931a0b1e3694
SamLaumans <1048784@hr.nl>
2025-10-31 12:29:57 +0100

    hallo

--- MobyPark/obj/Debug/net9.0/MobyPark.AssemblyInfo.cs
+++ MobyPark/obj/Debug/net9.0/MobyPark.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MobyPark")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99f61010cd95083fdc13f6de97d302d3c5f78254")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f9c0c4e22567ae5cc39c09e37e3bcd6321c1cc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MobyPark")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MobyPark")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
