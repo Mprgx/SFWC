0fda89d1d1c4dfab8900d41eb9ccc74dceea21fe
SamLaumans <1048784@hr.nl>
2025-10-31 12:09:52 +0100

    Revert "stage"
    This reverts commit 8020f6caf670ce94ea7f0787e23d31d13bd672a6.

--- MobyPark/obj/Debug/net9.0/MobyPark.AssemblyInfo.cs
+++ MobyPark/obj/Debug/net9.0/MobyPark.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MobyPark")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8020f6caf670ce94ea7f0787e23d31d13bd672a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99f61010cd95083fdc13f6de97d302d3c5f78254")]
 [assembly: System.Reflection.AssemblyProductAttribute("MobyPark")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MobyPark")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
