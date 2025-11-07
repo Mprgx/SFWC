08fa0dfae2a7f248d3093a1066cf867a5c3c7070
Mex <1079546@hr.nl>
2025-11-07 09:15:37 +0100

    push ci-cd

--- Integratietests/conftest.py
+++ Integratietests/conftest.py
@@ -1,18 +1,22 @@
 import pytest
 import requests
 
-BASE_URL = "http://localhost:8000/"
+BASE_URL = "http://localhost:5280/"
+
 
 @pytest.fixture
 def user_session():
-    resp = requests.post(BASE_URL + "login", json={"username": "Mex", "password": "Smpl3Pw!"})
+    resp = requests.post(BASE_URL + "login",
+                         json={"username": "Mex", "password": "Smpl3Pw!"})
     resp.raise_for_status()
     token = resp.json()["session_token"]
     return {"url": BASE_URL, "username": "Mex", "session_token": token}
 
+
 @pytest.fixture
 def admin_session():
-    resp = requests.post(BASE_URL + "login", json={"username": "Job", "password": "sendhelp"})
+    resp = requests.post(BASE_URL + "login",
+                         json={"username": "Job", "password": "sendhelp"})
     resp.raise_for_status()
     token = resp.json()["session_token"]
     return {"url": BASE_URL, "username": "Job", "session_token": token}
