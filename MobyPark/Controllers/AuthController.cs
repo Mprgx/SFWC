3a7c5bca59bec3bbc6d3e260536fd8e77e72514d
Mex <1079546@hr.nl>
2025-11-07 10:42:37 +0100

    Change api structure

--- MobyPark/Controllers/AuthController.cs
+++ MobyPark/Controllers/AuthController.cs
@@ -6,7 +6,7 @@ using System.Security.Claims;
 
 namespace MobyPark.Controllers
 {
-    [Route("api/")]
+    [Route("api")]
     [ApiController]
     public class AuthController(IAuthService authService) : ControllerBase
     {
