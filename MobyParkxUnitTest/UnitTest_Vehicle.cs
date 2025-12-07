c8ab78c4a4c005cd2bd762aa28100eb11904cbff
TigranesTG <job.limbeek@gmail.com>
2025-12-07 13:13:18 +0100

    unittests work now

--- MobyParkxUnitTest/UnitTest_Vehicle.cs
+++ MobyParkxUnitTest/UnitTest_Vehicle.cs
@@ -9,7 +9,7 @@ using Xunit;
 
 namespace MobyParkxUnitTest
 {
-    public class PostVehicleServiceTests
+    public class VehicleServiceTests
     {
         private static UserDbContext CreateDbContext(string dbName)
         {
@@ -20,11 +20,11 @@ namespace MobyParkxUnitTest
             return new UserDbContext(options);
         }
 
-        private async Task SeedVehicle(UserDbContext db, Guid userId, string plate)
+        private async Task SeedVehicle(UserDbContext db, Guid userId, string plate, int id)
         {
             db.Vehicles.Add(new Vehicle
             {
-                Id = 9999,
+                Id = id,
                 UserId = userId,
                 LicensePlate = plate,
                 Make = "Test",
@@ -43,7 +43,7 @@ namespace MobyParkxUnitTest
         public async Task CreateVehicleAsync_ReturnsVehicle_WhenNewForUser()
         {
             // Arrange
-            var db = CreateDbContext();
+            var db = CreateDbContext(nameof(CreateVehicleAsync_ReturnsVehicle_WhenNewForUser));
             var service = new VehicleService(db);
 
             var userId = Guid.NewGuid();
@@ -74,13 +74,13 @@ namespace MobyParkxUnitTest
         public async Task CreateVehicleAsync_ReturnsNull_WhenLicensePlateAlreadyExistsForUser()
         {
             // Arrange
-            var db = CreateDbContext();
+            var db = CreateDbContext(nameof(CreateVehicleAsync_ReturnsNull_WhenLicensePlateAlreadyExistsForUser));
             var service = new VehicleService(db);
 
             var userId = Guid.NewGuid();
-            await SeedVehicle(db, userId, "AA-123-AA");
+            await SeedVehicle(db, userId, "AA-123-AA", 9999);
 
-            var dto = new VehicleRequestDto
+            var dto = new VehicleCreateDto
             {
                 LicensePlate = "AA-123-AA",
                 Make = "BMW",
@@ -102,15 +102,15 @@ namespace MobyParkxUnitTest
         public async Task CreateVehicleAsync_CreatesVehicle_WhenPlateExistsButDifferentUser()
         {
             // Arrange
-            var db = CreateDbContext();
+            var db = CreateDbContext(nameof(CreateVehicleAsync_CreatesVehicle_WhenPlateExistsButDifferentUser));
             var service = new VehicleService(db);
 
             var user1 = Guid.NewGuid();
             var user2 = Guid.NewGuid();
 
-            await SeedVehicle(db, user1, "AA-123-AA");
+            await SeedVehicle(db, user1, "AA-123-AA", 9999);
 
-            var dto = new VehicleRequestDto
+            var dto = new VehicleCreateDto
             {
                 LicensePlate = "AA-123-AA",
                 Make = "Audi",
@@ -127,13 +127,109 @@ namespace MobyParkxUnitTest
             Assert.Equal(user2, result.UserId);
         }
 
+        // GET
+        // POSITIEF - ALLE VEHICLES VAN USER WORDEN OPGEHAALD
+        [Fact]
+        public async Task GetVehiclesForUserAsync_ReturnsVehicles_WhenUserHasVehicles()
+        {
+            // Arrange
+            var db = CreateDbContext(nameof(GetVehiclesForUserAsync_ReturnsVehicles_WhenUserHasVehicles));
+            var service = new VehicleService(db);
+
+            var userId = Guid.NewGuid();
+            await SeedVehicle(db, userId, "QQ-111-QQ", 9999);
+            await SeedVehicle(db, userId, "YY-222-YY", 8888);
+
+            // Act
+            var result = await service.GetVehiclesForUserAsync(userId);
+
+            // Assert
+            Assert.Equal(2, result.Count);
+            Assert.All(result, v => Assert.Equal(userId, v.UserId));
+        }
+
+        // GET
+        // NEGATIEF - USER HEEFT GEEN VEHICLES
+        [Fact]
+        public async Task GetVehiclesForUserAsync_ReturnsEmptyList_WhenUserHasNoVehicles()
+        {
+            // Arrange
+            var db = CreateDbContext(nameof(GetVehiclesForUserAsync_ReturnsEmptyList_WhenUserHasNoVehicles));
+            var service = new VehicleService(db);
+
+            var userId = Guid.NewGuid();
+
+            // Act
+            var result = await service.GetVehiclesForUserAsync(userId);
+
+            // Assert
+            Assert.Empty(result);
+        }
+
+        // PUT
+        // POSITIEF - VEHICLE WORDT GEUPDATE
+        [Fact]
+        public async Task UpdateVehicleAsync_UpdatesVehicle_WhenOwnedByUser()
+        {
+            // Arrange
+            var db = CreateDbContext(nameof(UpdateVehicleAsync_UpdatesVehicle_WhenOwnedByUser));
+            var service = new VehicleService(db);
+
+            var userId = Guid.NewGuid();
+            await SeedVehicle(db, userId, "AA-123-AA", 9999);
+
+            var updateDto = new VehicleUpdateDto
+            {
+                Make = "UpdatedMake",
+                Model = "UpdatedModel",
+                Color = "Black",
+                Year = 2024
+            };
+
+            // Act
+            var result = await service.UpdateVehicleAsync(userId, 9999, updateDto);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal("UpdatedMake", result.Make);
+            Assert.Equal("UpdatedModel", result.Model);
+            Assert.Equal("Black", result.Color);
+            Assert.Equal(2024, result.Year);
+        }
+
+        // PUT
+        // NEGATIEF - VEHICLE BESTAAT NIET OF NIET VAN USER
+        [Fact]
+        public async Task UpdateVehicleAsync_ReturnsNull_WhenVehicleNotOwned()
+        {
+            // Arrange
+            var db = CreateDbContext(nameof(UpdateVehicleAsync_ReturnsNull_WhenVehicleNotOwned));
+            var service = new VehicleService(db);
+
+            var ownerId = Guid.NewGuid();
+            var otherUserId = Guid.NewGuid();
+
+            await SeedVehicle(db, ownerId, "AA-123-AA", 9999);
+
+            var updateDto = new VehicleUpdateDto
+            {
+                Make = "NewMake"
+            };
+
+            // Act
+            var result = await service.UpdateVehicleAsync(otherUserId, 9999, updateDto);
+
+            // Assert
+            Assert.Null(result);
+        }
+
         // DELETE
         // POSITIEF - VEHICLE WORDT VERWIJDERD
         [Fact]
         public async Task DeleteVehicleAsync_ShouldDeleteVehicle_WhenOwnedByUser()
         {
             // Arrange
-            var context = CreateDbContext();
+            var context = CreateDbContext(nameof(DeleteVehicleAsync_ShouldDeleteVehicle_WhenOwnedByUser));
             var service = new VehicleService(context);
 
             var userId = Guid.NewGuid();
@@ -165,7 +261,7 @@ namespace MobyParkxUnitTest
         public async Task DeleteVehicleAsync_ShouldReturnFalse_WhenVehicleDoesNotExist()
         {
             // Arrange
-            var context = CreateDbContext();
+            var context = CreateDbContext(nameof(DeleteVehicleAsync_ShouldReturnFalse_WhenVehicleDoesNotExist));
             var service = new VehicleService(context);
 
             var userId = Guid.NewGuid();
@@ -183,7 +279,7 @@ namespace MobyParkxUnitTest
         public async Task DeleteVehicleAsync_ShouldReturnFalse_WhenVehicleNotOwnedByUser()
         {
             // Arrange
-            var context = CreateDbContext();
+            var context = CreateDbContext(nameof(DeleteVehicleAsync_ShouldReturnFalse_WhenVehicleNotOwnedByUser));
             var service = new VehicleService(context);
 
             var ownerId = Guid.NewGuid();
