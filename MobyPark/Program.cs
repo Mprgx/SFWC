fe7b50ac6786c0f21c2a8f4464f602aa6aa3b90f
Job Limbeek <Job.Limbeek@gmail.com>
2025-12-16 15:38:13 +0100

    workflow update

--- MobyPark/Program.cs
+++ MobyPark/Program.cs
@@ -117,7 +117,7 @@ if (app.Environment.IsDevelopment())
     app.MapScalarApiReference();
 }
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
