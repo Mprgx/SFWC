beaf3c2fca4197ae8e2dbb8c64ca2fa080e44605
Sharad Randjitsing <jurianrandjitsing@gmail.com>
2025-12-23 06:46:43 -0300

    completedVehicleOverhaul

--- MobyPark/Services/IVehicleService.cs
+++ MobyPark/Services/IVehicleService.cs
@@ -4,13 +4,13 @@ namespace MobyPark.Services
 {
     public interface IVehicleService
     {
-        Task<VehicleReadDto?> CreateVehicleAsync(Guid userId, VehicleCreateDto request);
-        Task<List<VehicleReadDto>> GetVehiclesForUserAsync(Guid userId);
-        Task<List<VehicleReadDto>> GetVehiclesByUsernameAsync(string username);
-        Task<VehicleReadDto?> UpdateVehicleAsync(Guid userId, int vehicleId, VehicleUpdateDto request);
-        Task<bool> DeleteVehicleAsync(Guid userId, int vehicleId);
-        Task<List<VehicleHistoryDto>> GetVehicleHistoryAsync(int vehicleId);
-
+        Task<(VehicleReadDto? dto, string? error, int? status)> CreateVehicleAsync(Guid userId, VehicleCreateDto request);
+        Task<(List<VehicleReadDto>? dtos, string? error, int? status)> GetVehiclesForUserAsync(Guid userId);       
+        Task<(VehicleReadDto? dto, string? error, int? status)> UpdateVehicleAsync(Guid userId, int vehicleId, VehicleUpdateDto request);
+        Task<(string? error, int? status)> DeleteVehicleAsync(Guid userId, int vehicleId);
+        Task<(List<VehicleReadDto>? dtos, string? error, int? status)> GetVehiclesByUsernameAsync(string username);
+        Task<(List<VehicleHistoryDto>? dtos, string? error, int? status)> GetVehicleHistoryAsync(Guid userId, int vehicleId);
+        Task<(List<VehicleHistoryDto>? dtos, string? error, int? status)> GetVehicleHistoryAdminAsync(int vehicleId);
     }
 
 
