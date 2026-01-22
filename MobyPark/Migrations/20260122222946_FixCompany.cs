8e9d507c2edf2fc67654b3fb7447999b4da1b882
Mex Punt <159786543+Mprgx@users.noreply.github.com>
2026-01-22 23:30:00 +0100

    FixCompanyDb

---
+++ MobyPark/Migrations/20260122222946_FixCompany.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MobyPark.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixCompany : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CompanyUsers_Companies_CompanyId",
+                table: "CompanyUsers");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CompanyUsers_Companies_CompanyId",
+                table: "CompanyUsers",
+                column: "CompanyId",
+                principalTable: "Companies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CompanyUsers_Companies_CompanyId",
+                table: "CompanyUsers");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CompanyUsers_Companies_CompanyId",
+                table: "CompanyUsers",
+                column: "CompanyId",
+                principalTable: "Companies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
